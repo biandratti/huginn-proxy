2afa5fd14edce3ba58ce2d9ec947d87535feb828
biandratti <maxibiandra@gmail.com>
2025-12-29 11:17:06 +0100

    fmt fixed

--- huginn-proxy-lib/tests/proxy/http_result.rs
+++ huginn-proxy-lib/tests/proxy/http_result.rs
@@ -2,15 +2,9 @@ use huginn_proxy_lib::proxy::http_result::HttpError;
 
 #[test]
 fn test_error_type_mapping() {
-    assert_eq!(
-        HttpError::InvalidHostInRequestHeader.error_type(),
-        "invalid_host"
-    );
+    assert_eq!(HttpError::InvalidHostInRequestHeader.error_type(), "invalid_host");
     assert_eq!(HttpError::NoMatchingBackend.error_type(), "no_matching_backend");
-    assert_eq!(
-        HttpError::NoUpstreamCandidates.error_type(),
-        "no_upstream_candidates"
-    );
+    assert_eq!(HttpError::NoUpstreamCandidates.error_type(), "no_upstream_candidates");
     assert_eq!(
         HttpError::FailedToGenerateUpstreamRequest("test".to_string()).error_type(),
         "upstream_request_failed"
@@ -32,21 +26,11 @@ fn test_status_code_conversion() {
     use http::StatusCode;
     use std::convert::From;
 
-    assert_eq!(
-        StatusCode::from(HttpError::InvalidHostInRequestHeader),
-        StatusCode::BAD_REQUEST
-    );
-    assert_eq!(
-        StatusCode::from(HttpError::NoMatchingBackend),
-        StatusCode::SERVICE_UNAVAILABLE
-    );
-    assert_eq!(
-        StatusCode::from(HttpError::NoUpstreamCandidates),
-        StatusCode::NOT_FOUND
-    );
+    assert_eq!(StatusCode::from(HttpError::InvalidHostInRequestHeader), StatusCode::BAD_REQUEST);
+    assert_eq!(StatusCode::from(HttpError::NoMatchingBackend), StatusCode::SERVICE_UNAVAILABLE);
+    assert_eq!(StatusCode::from(HttpError::NoUpstreamCandidates), StatusCode::NOT_FOUND);
     assert_eq!(
         StatusCode::from(HttpError::FailedToGetResponseFromBackend("test".to_string())),
         StatusCode::BAD_GATEWAY
     );
 }
-
